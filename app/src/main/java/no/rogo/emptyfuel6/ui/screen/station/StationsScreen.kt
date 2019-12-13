caeefa94018d6d7a1a3996b0c0d7a0fe6c59231a
Roar <roar.gronmo@gmai.com>
2019-12-13 14:47:32 +0100

    Update, more layout changes, one problem left...

--- app/src/main/java/no/rogo/emptyfuel6/ui/screen/station/StationsScreen.kt
+++ app/src/main/java/no/rogo/emptyfuel6/ui/screen/station/StationsScreen.kt
@@ -7,22 +7,27 @@ import androidx.compose.frames.open
 import androidx.compose.unaryPlus
 import androidx.core.content.ContextCompat
 import androidx.ui.core.*
+import androidx.ui.engine.geometry.Offset
 import androidx.ui.foundation.DrawImage
 import androidx.ui.foundation.VerticalScroller
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.graphics.Color
 import androidx.ui.graphics.Image
+import androidx.ui.graphics.Shadow
 import androidx.ui.layout.*
 import androidx.ui.material.*
 import androidx.ui.material.surface.Card
 import androidx.ui.material.surface.Surface
 import androidx.ui.res.imageResource
 import androidx.ui.res.vectorResource
+import androidx.ui.text.ParagraphStyle
 import androidx.ui.text.font.FontFamily
 import androidx.ui.text.font.FontWeight
+import androidx.ui.text.style.TextAlign
 import androidx.ui.text.style.TextOverflow
 import androidx.ui.tooling.preview.Preview
 import no.rogo.emptyfuel6.R
+import no.rogo.emptyfuel6.ui.main.VectorImage
 import no.rogo.emptyfuel6.ui.main.VectorImageButton
 
 /**
@@ -55,6 +60,7 @@ fun StationsScreen(
             Column(
                     modifier = Expanded
             ) {
+                //CardGravityTest()
                 for (i in 0 until 10)
                 {
                     StationCard()
@@ -64,12 +70,101 @@ fun StationsScreen(
     }
 }
 
+@Composable
+fun CardGravityTest()
+{
+    Card(
+            modifier = Spacing(all = 4.dp),
+            color = Color.LightGray,
+            shape = RoundedCornerShape(
+                    5.dp
+            )
+    ){
+        Column {
+            Row {
+                Surface(color = Color.Red,
+                        modifier = Spacing(all = 4.dp)) {
+                    Text(text = "1 XX")
+                }
+                Column(modifier = Flexible(1f) wraps Expanded wraps Spacing(4.dp)){
+                    Surface(color = Color.Green){
+                        Text(text="2 Flexible wraps Expanded(1f) wraps Spacing(4.dp) x x xx xxx xxxx x xx xxx x xxx")
+                    }
+                    Surface(color = Color.Magenta){
+                        Text(text = "3 Second line xxxxx xxx xx xxxxx xxxx")
+                    }
+                }
+                Column(modifier = ExpandedHeight) {
+                    Surface(color = Color.DarkGray) {
+                        Text(text = "4 Column: OK High on top!")
+                    }
+
+                    Row(modifier =Expanded wraps Gravity.End) {
+
+                        Column(modifier = Gravity.Center wraps Expanded) {
+                            Surface(color = Color.Yellow,
+                                    modifier = Gravity.End){
+                                Text(text = "5 Center End me!",
+                                        modifier = Gravity.End)
+                            }
+                        }
+                    }
+
+
+                }
+            }
+
+            Divider()
+
+            Row (modifier = Spacing(top = 4.dp)){
+                Column (modifier = Flexible(1f) wraps Expanded){
+                    Surface(color = Color.Blue, modifier = Gravity.Start) {
+                        Text(text = "6 Row spacing top, Column Flexible(1f) wraps Expanded, Surface gravity start")
+                    }
+                }
+                Column(modifier = Flexible(1f) wraps Expanded){
+                    Surface(color = Color.Cyan, modifier = Gravity.End) {
+                        Text(text = "7 Column Flexible wraps Expanded, Surface gravity end, " +
+                                "paragraphstyle TextAlign.end ", paragraphStyle = ParagraphStyle(textAlign = TextAlign.End))
+                    }
+                }
+            }
+
+            Row {
+                Surface(modifier = Spacing(all = 4.dp), color = Color.White) {
+                    Text(text = "8 on Row")
+                }
+                Surface(modifier = Spacing(all = 4.dp), color = Color.Green)
+                {
+                    Text(text = "9 on Row")
+                }
+                Column(modifier = Gravity.Center wraps Flexible(1f)){
+                    Surface(color = Color.Red) {
+                        Text(text = "10 col center flex, line 1")
+                    }
+                    Surface(color = Color.DarkGray)
+                    {
+                        Text(text = "11 line 2 xxxxxx")
+                    }
+                }
+                Column(modifier = Gravity.Center wraps Expanded) {
+                    Surface(color = Color.Magenta, modifier = Gravity.End) {
+                        Text(text = "12 col cnt mod end")
+                    }
+                }
+            }
+
+
+        }
+    }
+}
 
 @Composable
 fun StationCard()
 {
 
     val image = +imageResource(R.drawable.yx_icon)
+    val imagekind = +imageResource(R.drawable.ic_empty_kind_24dp_black)
 
     Card(
             modifier = Spacing(
@@ -81,7 +176,9 @@ fun StationCard()
             )
     ) {
         Column {
-            Row(){
+            //Station info Row
+            Row{
+                //Enterprise icon
                 Surface(shape = RoundedCornerShape(size = 4.dp),
                         modifier = Spacing(all = 4.dp) ,
                         color = Color.White.copy(alpha = 0.60f)
@@ -93,31 +190,170 @@ fun StationCard()
                              containerHeight = 40.dp
                      )
                 }
-
+                //Station info
                 Column(modifier = Flexible(1f) wraps Expanded) {
+                    //Station name
                     Surface(color = Color.Transparent) {
                         Text(
-                                text = "Station Name",
-                                style = ((+MaterialTheme.typography()).h6),
+                                text = "Station Name name namne namen mname naneme",
+                                style = ((+MaterialTheme.typography()).h6).withOpacity(0.85f),
                                 maxLines = 2,
                                 overflow = TextOverflow.Ellipsis
                         )
                     }
-                    Surface(color = Color.Yellow){
+                    //Station address
+                    Surface(color = Color.Transparent){
                         Text(
-                                text = "Station Address, 0000 Location",
+                                text = "Station Address, 0000 lon glon glong lomn glom gLocation",
                                 style = ((+MaterialTheme.typography()).subtitle2).withOpacity(0.6f),
                                 maxLines = 2,
                                 overflow = TextOverflow.Ellipsis
                         )
                     }
                 }
-
+                //Distance and Details button column
                 Column(modifier = Spacing(right = 4.dp) )
                 {
-                    Surface(color = Color.Transparent, modifier = Gravity.End) {
+                    //Station distance
+                    Surface(color = Color.Transparent, modifier = Gravity.End wraps Expanded) {
                         Text(
-                                text = "123,3 km",
+                                text = "123000000000,3 km",
+                                style = ((+MaterialTheme.typography()).caption)
+                                        .copy(
+                                                fontFamily = FontFamily.Monospace,
+                                                fontWeight = FontWeight.Bold,
+                                                color = Color.Black
+                                        ).withOpacity(0.6f)
+                        )
+                    }
+
+                    //Details button
+                    Row(modifier = Gravity.End wraps ExpandedHeight
+                    ){
+                        Column(modifier = Gravity.Center wraps ExpandedHeight) {
+                            Surface(color = Color.Transparent,
+                                    modifier = Gravity.End) {
+                                Button(
+                                        onClick = {},
+                                        text = "DETAILS",
+                                        style = TextButtonStyle()
+                                )
+                            }
+                        }
+                    }
+
+
+                }
+
+            }
+            //Horizontal divider
+            Divider(
+                    modifier = Spacing(
+                            left = 4.dp,
+                            right = 4.dp),
+                    color = Color.Black
+            )
+
+            Row{
+                //Update since
+                Column(modifier = Flexible(1f) wraps Expanded) {
+                    Surface(
+                            color = Color.Transparent,
+                            modifier = Spacing(
+                                    left = 4.dp
+                            ) wraps Gravity.Start ) {
+                        Text(
+                                text = "9h 4m",
+                                modifier = Gravity.Start,
+                                paragraphStyle = ParagraphStyle(textAlign = TextAlign.Start),
+                                style = ((+MaterialTheme.typography()).caption)
+                                        .copy(
+                                                fontFamily = FontFamily.Monospace,
+                                                fontWeight = FontWeight.Bold,
+                                                color = Color.Black
+                                        ).withOpacity(0.6f)
+                        )
+                    }
+                }
+                //Update timestamp
+                Column(modifier = Flexible(1f) wraps Expanded){
+                    Surface(
+                            color = Color.Transparent,
+                            modifier = Spacing(
+                                    right = 4.dp
+                            ) wraps Gravity.End) {
+                        Text(
+                                text = "2019-12-12 12:13:39",
+                                modifier = Gravity.End,
+                                paragraphStyle = ParagraphStyle(textAlign = TextAlign.End),
+                                style = ((+MaterialTheme.typography()).caption)
+                                        .copy(
+                                                fontFamily = FontFamily.Monospace,
+                                                fontWeight = FontWeight.Bold,
+                                                color = Color.Black
+                                        ).withOpacity(0.6f)
+                        )
+                    }
+                }
+            }
+            Row {
+                //Kind icon
+                Surface(shape = RoundedCornerShape(size = 4.dp),
+                        modifier = Spacing(all = 4.dp) ,
+                        color = Color.White.copy(alpha = 0.60f)
+                ) {
+                    DrawImageFitCenter(
+                            image = imagekind,
+                            modifier = Spacing(all = 4.dp),
+                            containerWidth = 40.dp,
+                            containerHeight = 40.dp
+                    )
+                }
+
+                //Price text
+                Surface(
+                        color = Color.Transparent,
+                        modifier = Spacing(all = 4.dp)
+                ) {
+                    Text(
+                            text = "15,98",
+                            style = ((+MaterialTheme.typography()).h4)
+                                    .copy(fontFamily = FontFamily.Monospace,
+                                            fontWeight = FontWeight.Bold,
+                                            color = Color.Yellow,
+                                            shadow = Shadow(
+                                                    color = Color.Gray,
+                                                    offset = Offset(
+                                                            dx = 5f,
+                                                            dy = 5f
+                                                    ),
+                                                    blurRadius = 2.px
+                                            )
+                                    ),
+                            maxLines = 1
+                    )
+                }
+
+                //Currency and quantity measurements
+                Column(
+                        modifier = Gravity.Center wraps Flexible(1f)
+                ){
+                    //Currency
+                    Surface(color = Color.Transparent) {
+                        Text(
+                                text = "NOK",
+                                style = ((+MaterialTheme.typography()).caption)
+                                        .copy(
+                                                fontFamily = FontFamily.Monospace,
+                                                fontWeight = FontWeight.Bold
+                                )
+                        )
+                    }
+
+                    //Quantity measurements
+                    Surface(color = Color.Transparent) {
+                        Text(
+                                text = "ltr",
                                 style = ((+MaterialTheme.typography()).caption)
                                         .copy(
                                                 fontFamily = FontFamily.Monospace,
@@ -125,39 +361,27 @@ fun StationCard()
                                         )
                         )
                     }
-                    /*
-                    Surface(color = Color.Red, modifier = Gravity.End) {
+                }
 
-                        Text(text = "14d 16h 00min")
-                    }*/
-
-                    Surface(color = Color.Transparent) {
+                //Update Button
+                Column(modifier = Gravity.Center wraps Expanded) {
+                    Surface(color = Color.Transparent,
+                            modifier = Spacing(right = 4.dp) wraps Gravity.End) {
                         Button(
                                 onClick = {},
-                                text = "DETAILS",
-                                style = TextButtonStyle()
-                        )
-                    }
-
-                }
-
-            }
-            Divider(modifier = Spacing(left = 4.dp, right = 4.dp))
-            Row(){
-                Column(modifier = Flexible(1f) wraps Expanded) {
-                    Surface(
-                            color = Color.White,
-                            modifier = Spacing(
-                                    left = 4.dp
-                            ) wraps Gravity.Start ) {
-                        Text(text = "Country average",
-                                style = ((+MaterialTheme.typography()).subtitle2)
-                                        .copy(
-                                                fontFamily = FontFamily.Monospace,
-                                                fontWeight = FontWeight.Bold
-                                        ))
+                                style = ContainedButtonStyle()
+                        ){
+                            Row() {
+                                VectorImage(id = R.drawable.ic_baseline_edit_24)
+                                WidthSpacer(width = 16.dp)
+                                Text(modifier = Gravity.Center,
+                                        text = "UPDATE",
+                                        style = ((+MaterialTheme.typography()).button))
+                            }
+                        }
                     }
                 }
+
             }
         }
     }
@@ -210,7 +434,7 @@ fun PreviewStationsCard()
 fun TemplatePreviewStationsCard(
 
 ){
-    MaterialTheme() {
-        StationCard()
+    MaterialTheme {
+        CardGravityTest()
     }
 }
